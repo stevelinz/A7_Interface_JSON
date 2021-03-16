d1bdef812b40a15ced6fd8714f50f42c54ebecdf
stevelinz <65718928+stevelinz@users.noreply.github.com>
2021-03-16 13:19:38 -0500

    Adding the Program files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+
+
+namespace A4___Movie_Library_Assignment_LINZ
+{
+     class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            {
+                ActionSelected action = new ActionSelected();
+                action.selectAction();            
+            }
+        }
+    }
+}
